a2deb05256499c2cc7e0a395d599f103347bfe4b
Nikola Vrhovac <vrllee@gmail.com>
2021-10-23 21:03:32 +0200

    basic server rendered components

---
+++ client/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const App = ({ questions, answers }) => {
+	return (
+		<div>
+			<h1>Q&A Tool</h1>
+			{questions.map(({ questionId, content }) => (
+				<div key={questionId}>
+					<h3>{content}</h3>
+					<div>
+						{answers
+							.filter(answer => answer.questionId === questionId)
+							.map(({ content, upvotes, answerId }) => (
+								<div key={answerId}>
+									<span>
+										{content} - {upvotes}
+									</span>
+								</div>
+							))}
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,51 @@
+import React from 'react';
 import express from 'express';
 import { readFileSync } from 'fs';
+import { renderToString } from 'react-dom/server';
+
+import { App } from '../client/App';
+
+const data = {
+	questions: [
+		{ questionId: 'Q1', content: 'Should we use jQuery or fetch for Ajax?' },
+		{ questionId: 'Q2', content: 'What is the best feature of React?' },
+	],
+	answers: [
+		{
+			answerId: 'A1',
+			questionId: 'Q1',
+			upvotes: 2,
+			content: 'jQuery',
+		},
+		{
+			answerId: 'A2',
+			questionId: 'Q1',
+			upvotes: 1,
+			content: 'Fetch',
+		},
+		{
+			answerId: 'A3',
+			questionId: 'Q2',
+			upvotes: 1,
+			content: 'Performance',
+		},
+		{
+			answerId: 'A4',
+			questionId: 'Q2',
+			upvotes: 2,
+			content: 'Hooks',
+		},
+	],
+};
 
 const app = new express();
 
 app.use(express.static('dist'));
 
 app.get('/', async (_req, res) => {
-	const index = readFileSync(`public/index.html`, 'utf8');
-	res.send(index);
+	const index = readFileSync('public/index.html', 'utf8');
+	const rendered = renderToString(<App {...data} />);
+	res.send(index.replace('{{rendered}}', rendered));
 });
 
 app.listen(7777);
